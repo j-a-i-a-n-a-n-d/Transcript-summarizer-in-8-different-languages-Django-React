128863769f5cc138476084aac48f0fb1837911cf
jai <jaianand5789@gmail.com>
2023-07-12 04:59:47 +0530

    integration

---
+++ chromeextension/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MyForm from './MyForm';
+
+function App() {
+  return (
+    <div className='App'>
+      YT Transcript Summarizer
+      <MyForm />
+    </div>
+  );
+}
+
+export default App;
